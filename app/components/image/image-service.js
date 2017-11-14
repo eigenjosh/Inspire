e795b1a831e6ba7af552b59bb17e948e07e88fe3
Josh Eigen <josh.eigen@gmail.com>
2017-11-13 18:53:41 -0800

    image filtering for high res photo url

--- app/components/image/image-service.js
+++ app/components/image/image-service.js
@@ -1,13 +1,17 @@
 function ImageService() {
 	var url = '//bcw-getter.herokuapp.com/?url=';
-	var url2 = 'http://www.splashbase.co/api/v1/images/random'
-	var apiUrl = url + encodeURIComponent(url2);
+	var url2 = 'http://www.splashbase.co/api/v1/images/random';
+	var apiUrl = url + encodeURIComponent(url2)
 
 	this.getImage = function (callWhenDone) {
 		// ^^^^^^^ How do you call this function?
 		return $.get(apiUrl, function (res) {
 			res = JSON.parse(res)
 			callWhenDone(res)
+			if(!res.large_url){
+				console.log('No high-resolution photo available. Fetching new url...')
+				ImageService()
+			} 
 		})
 	}
 }

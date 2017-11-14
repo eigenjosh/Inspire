0cd50cdc19c210dc9ee33bab715700feb4e9ddbb
Josh Eigen <josh.eigen@gmail.com>
2017-11-13 19:51:50 -0800

    add task button functional

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -3,7 +3,6 @@ function ImageController() {
 	var imageService = new ImageService();
 
 	imageService.getImage(function (image) {
-		debugger
 		var largeUrl = image.large_url
 		if(largeUrl !== null) {
 			document.body.style.backgroundImage = `url(${image.large_url})`
